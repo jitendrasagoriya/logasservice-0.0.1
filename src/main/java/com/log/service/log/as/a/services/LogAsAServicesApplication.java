3d6377823522cd65cc78bc7dc5e2c3a1b404b73c
jitendrasagoriya <jitendrasagoriya83.gmail.com>
2017-09-12 21:17:16 +0530

    first commit

---
+++ src/main/java/com/log/service/log/as/a/services/LogAsAServicesApplication.java
@@ -0,0 +1,12 @@
+package com.log.service.log.as.a.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAsAServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAsAServicesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/log/service/log/as/a/services/LogAsAServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.log.service.log.as.a.services;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LogAsAServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
